ed07ec6508092f001d55fb8c40bfb667099747fb
Lupesms97 <lupesms97@gmail.com>
2023-01-20 15:32:24 -0300

    Criando classes Vagas e Match

--- src/main/java/org/example/controller/ClienteRepository.java
+++ src/main/java/org/example/controller/ClienteRepository.java
@@ -2,4 +2,5 @@ package org.example.controller;
 
 public class ClienteRepository {
 
+
 }
---
+++ src/main/java/org/example/model/Match.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+abstract class Match {
+    public void Like(){}
+    public void Deslike(){}
+    public void Match(){}
+    public void EnviarMensagem(){}
+}
